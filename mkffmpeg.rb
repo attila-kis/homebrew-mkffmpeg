5939976c1f2e760cee79a14efee759b3451d4dbf
attila-kis <87068871+attila-kis@users.noreply.github.com>
2021-07-08 12:12:59 +0200

    Create mkffmpeg.rb

---
+++ mkffmpeg.rb
@@ -0,0 +1,78 @@
+# Last check with upstream: b99c1053f197ee0662e1b34664077784dd2d5432
+# https://github.com/Homebrew/homebrew-core/blob/master/Formula/ffmpeg.rb
+
+class Mkffmpeg < Formula
+  desc "Play, record, convert, and stream audio and video"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-4.4.tar.xz"
+  sha256 "06b10a183ce5371f915c6bb15b7b1fffbe046e8275099c96affc29e17645d909"
+  head "https://github.com/FFmpeg/FFmpeg.git"
+
+  keg_only <<EOS
+it is intended to only be used for building IINA.
+This formula is not recommended for daily use and has no binaraies (ffmpeg, ffplay etc.)
+EOS
+  
+  depends_on "nasm" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texi2html" => :build
+
+  depends_on "aom"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "frei0r"
+  depends_on "openssl"
+  depends_on "libass"
+  depends_on "libbluray"
+  depends_on "libsoxr"
+  depends_on "rtmpdump"
+  depends_on "rubberband"
+  depends_on "snappy"
+  depends_on "speex"
+  depends_on "tesseract"
+  depends_on "xz"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --enable-hardcoded-tables
+      --enable-avresample
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-ffplay
+      --enable-openssl
+      --enable-libaom
+      --enable-libbluray
+      --enable-libsnappy
+      --enable-libtesseract
+      --enable-libfontconfig
+      --enable-libfreetype
+      --enable-libass
+      --enable-librtmp
+      --enable-libspeex
+      --enable-videotoolbox
+      --disable-libjack
+      --disable-indev=jack
+      --enable-libsoxr
+      --disable-programs
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+
+    # Build and install additional FFmpeg tools
+    system "make", "alltools"
+    bin.install Dir["tools/*"].select { |f| File.executable? f }
+  end
+
+  test do
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    assert_predicate mp4out, :exist?
+  end
+end
